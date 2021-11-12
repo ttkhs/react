cb48bbd1f8132d7757fe1b739088d593136c8a9c
ttkhs <51502028+ttkhs@users.noreply.github.com>
2021-11-12 21:15:18 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => {
+    alert();
+  };
+
+  const contentStyle = {
+    color: "bleu",
+    fontSize: "18px"
+  };
+
   return (
     <>
-      <h1> aaa</h1>
+      <h1 style={contentStyle}> aaa</h1>
       <p>ooo</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
